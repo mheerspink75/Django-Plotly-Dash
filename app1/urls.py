61f88b188a6f47c75b9f10bac723601f5828d068
Matt Heerspink <mheerspink75@gmail.com>
2019-11-15 08:46:10 -0600

    fixed formatting

--- app1/urls.py
+++ app1/urls.py
@@ -6,5 +6,4 @@ from app1 import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  #path('django_plotly_dash/', include('django_plotly_dash.urls')),
 ]
