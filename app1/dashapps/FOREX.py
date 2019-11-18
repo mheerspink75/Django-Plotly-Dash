50ad8b2678d1f5b336503673f1e855b59fb36484
mheerspink75 <51008045+mheerspink75@users.noreply.github.com>
2019-11-17 23:47:38 -0600

    Update FOREX.py

--- app1/dashapps/FOREX.py
+++ app1/dashapps/FOREX.py
@@ -3,9 +3,7 @@ from API_KEYS import ALPHAVANTAGE_API_KEY
 
 API_KEY = 'ALPHAVANTAGE_API_KEY'
 
-
 def get_forex():
     return web.DataReader(["USD/BTC", "BTC/USD"], "av-forex", api_key=API_KEY)
-
 
 print(get_forex())
