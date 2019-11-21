9930c139d1c4119cb76012c294f89d1f445efa75
Matt Heerspink <mheerspink75@gmail.com>
2019-11-21 02:52:09 -0600

    updated settings

--- django_app1/settings.py
+++ django_app1/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .KEY_GEN import KEYGEN
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1'
+SECRET_KEY = 'KEYGEN'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
