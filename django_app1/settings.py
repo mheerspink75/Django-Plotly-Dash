de051a4cdc766da4b185b6528f36c7c9c16f350d
Matt Heerspink <mheerspink75@gmail.com>
2019-11-21 03:15:26 -0600

    added key generator

--- django_app1/settings.py
+++ django_app1/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from .KEY_GEN import KEYGEN
+from django.core.management.utils import get_random_secret_key
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'KEYGEN'
+SECRET_KEY = get_random_secret_key()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
