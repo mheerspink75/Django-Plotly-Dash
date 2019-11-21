3066f336cb3565e411d2b7691c0a346df3e6e8ad
Matt Heerspink <mheerspink75@gmail.com>
2019-11-21 01:41:05 -0600

    changed debug to false

--- django_app1/settings.py
+++ django_app1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b8svl9#t37gc)_tve!%+!=-g)zg^tdso$us=9p##g#pm!*a^m5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-machtrade.herokuapp.com', '127.0.0.1']
 
