1105a64dc2cba27a1a38632072e9a66c4ed88088
Matt Heerspink <mheerspink75@gmail.com>
2019-11-21 14:19:51 -0600

    updated staticfiles settings

--- django_app1/settings.py
+++ django_app1/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#y#)4x@4jdvy4sp4^afphmxgj02w7k+f^uk5rg#&pixh^fxm2l'
+SECRET_KEY = '#'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-machtrade.herokuapp.com', '127.0.0.1']
 
