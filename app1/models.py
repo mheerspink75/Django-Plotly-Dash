44a30551690ec4b64813195d98259e660a05d37b
Matt Heerspink <mheerspink75@gmail.com>
2019-11-22 16:01:46 -0600

    updated pages and models

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import (
+    AbstractBaseUser
+)
 
+class CustomUser(AbstractBaseUser):
+    pass
