0e4052f3e222979d3ad8167c365908afb46342e3
Matt Heerspink <mheerspink75@gmail.com>
2020-01-28 13:23:51 -0600

    addedndefault balance to models.py

--- app1/models.py
+++ app1/models.py
@@ -6,7 +6,7 @@ class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     account_balance = models.DecimalField(max_digits=12, decimal_places=2, null=True)
     account_date =  models.DateField(null=True, blank=True)
-    cash_balance = models.DecimalField(max_digits=12, decimal_places=2, null=True)
+    cash_balance = models.DecimalField(max_digits=12, decimal_places=2, default=100000)
     transaction_date = models.DateField(null=True, blank=True)
     transaction_type = models.CharField(max_length=30)
     transaction_amount = models.DecimalField(max_digits=12, decimal_places=2, null=True)
